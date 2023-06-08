a910fa89b0b65580fda722f5ecedb4fc31db76dc
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-06-08 16:26:19 +0300

    connection firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 
+import App from './components/App';
+
+import './firebase'
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
